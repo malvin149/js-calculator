e4809301369866003fd243ea20bc3a53db951339
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-15 20:11:02 +0000

    Feat(UI): Enable digit button input and display updates
    Connected number buttons to the calculator display.
    - Selected DOM elements for display and digit
    buttons.
    - Implemented `updateDisplay` helper function.
    - Added event listeners to number buttons to
    populate display.
    - Implemented logic to append digits or clear
    display for new input.
    - Added logic to prevent multiple decimal points.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// Variables
 let firstNumber; 
 let operator; 
 let secondNumber;
+let displayValue = '0'
+let shouldClearDisplay = false;
 
+// Get references to HTML elements
+const display = document.getElementById('display');
+const numberBtn = document.querySelectorAll('.number-button');
+
+
+
+// Helper function
+const updateDisplay = (value) => {
+    display.textContent = value;
+}
+
+// Basic arithmetic Funtions
 const add = (num1, num2) => {
     const result = num1 + num2;
     return result;
@@ -25,9 +40,11 @@ const divide = (num1, num2) => {
     return result;
 };
 
+
+// The Operate Function
 const operate = (operator, num1, num2) => {
     let result; 
-
+    
     switch (operator) {
         case '+':
             result = add(num1, num2);
@@ -48,3 +65,22 @@ const operate = (operator, num1, num2) => {
         return parseFloat(result.toFixed(8));
     }
 }
+
+// Event listeners
+    numberBtn.forEach(button => (
+        button.addEventListener('click', (event) => {
+            const digit = event.target.dataset.value;
+            
+            if (displayValue === '0' || shouldClearDisplay) {
+                displayValue = digit;
+            } else {
+                if (digit === '.' && displayValue.includes(('.'))) {
+                    return;
+                }
+                displayValue += digit;
+            }
+            updateDisplay(displayValue);
+        })
+    ))
+    
+    updateDisplay(displayValue);

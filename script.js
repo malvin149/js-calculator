5c3ae36240beb3b500264e82817b8131ba70cd1f
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-06-21 13:57:53 +0000

    Add math functions and operate dispatcher

---
+++ script.js
@@ -0,0 +1,23 @@
+const operate = (operator, firstNumber, secondNumber) => {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+            break;
+        case '-':
+            return subtract(firstNumber, secondNumber);
+            break;
+        case '*':
+            return multiply(firstNumber, secondNumber);
+            break;
+        case '/':
+            return divide(firstNumber, secondNumber);
+        break;
+    }
+}
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => {
+    return (b === 0) ? `Error` : a / b;
+};
